5f52895376c6eab89c26e96f7b1470a8abacb196
Eliah REBSTOCK <eliah.rebstock@ensiie.fr>
2016-12-08 09:28:21 +0100

    Add optional username argument

--- eduspot.py
+++ eduspot.py
@@ -9,15 +9,15 @@ def connect(username, password):
     s = requests.session()
 
     # Get the authentication token between the ENSIIE server and the captive portal
-    r = s.get("https://univnautes.ensiie.fr/sso?entity_id=https://shibboleth.ensiie.fr/idp/shibboleth")
+    r = s.get('https://univnautes.ensiie.fr/sso?entity_id=https://shibboleth.ensiie.fr/idp/shibboleth')
     soup = BeautifulSoup(r.text, 'html.parser')
     token1 = soup.find('input')['value']
 
     # Post it to ENSIIE server and retrieve ENSIIE CAS form
-    j = s.post("https://shibboleth.ensiie.fr/idp/profile/SAML2/POST/SSO", data = {"SAMLRequest": token1})
+    j = s.post('https://shibboleth.ensiie.fr/idp/profile/SAML2/POST/SSO', data = {'SAMLRequest': token1})
     soup = BeautifulSoup(j.text, 'html.parser')
-    urlpost = "https://cas.ensiie.fr" + soup.find('form')['action']
-    print("Remplissage du formulaire de connexion")
+    urlpost = 'https://cas.ensiie.fr' + soup.find('form')['action']
+    print('Remplissage du formulaire de connexion')
 
     # Retrieve hidden inputs to post again
     inputs = soup.find_all('input')
@@ -29,9 +29,12 @@ def connect(username, password):
 
     # While authentication is not done
     while (True):
+        # Ask for username if not given in parameter
+        if not username:
+            username = input("Nom d'utilisateur : ")
         # Ask for password if not given in parameter
         if not password:
-            password = getpass.getpass("Entrez votre mot de passe : ")
+            password = getpass.getpass('Entrez votre mot de passe : ')
 
         # Authentication to ENSIIE CAS
         payload = {'_eventId': 'submit', 'lt': lt, 'execution':execution, 'submit': 'LOGIN', 'username': username, 'password': password}
@@ -40,25 +43,25 @@ def connect(username, password):
 
         # Check if the authentication failed or not
         if soup.title and 'CAS' in soup.title.text:
-            password = ""
+            password = ''
         else:
             break
 
     # Retrieve the response token to send back to captive portal
     token2 = soup.find('input')['value']
 
-    print("Authentification finale au portail captif")
-    s.post("https://univnautes.ensiie.fr/authsaml2/singleSignOnPost", {"SAMLResponse": token2})
+    print('Authentification finale au portail captif')
+    s.post('https://univnautes.ensiie.fr/authsaml2/singleSignOnPost', {'SAMLResponse': token2})
 
     # Finally, make a test with a classic test website
-    print("Vérification avec captive.apple.com")
-    r = s.get("http://captive.apple.com")
-    if "Success" in r.text:
-        print("Vérification terminée : vous êtes connecté à eduspot !")
+    print('Vérification avec captive.apple.com')
+    r = s.get('http://captive.apple.com')
+    if 'Success' in r.text:
+        print('Vérification terminée : vous êtes connecté à eduspot !')
 
 parser = argparse.ArgumentParser(description='Connect to eduspot.')
-parser.add_argument("username", help="Username of the user")
-parser.add_argument("-p", "--password", help="Password of the user")
+parser.add_argument('-u', '--username', help='Username of the user')
+parser.add_argument('-p', '--password', help='Password of the user')
 args = parser.parse_args()
 
 connect(args.username, args.password)
